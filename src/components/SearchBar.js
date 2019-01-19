f5bf6ce43810c2f09fd23353b5203d54eb6e2008
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-18 22:37:32 -0400

    search bar showing and API key ready

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,30 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+    state = { term: '' }
+
+    onInputChange = (e) => {
+        this.setState({term:e.target.value});
+    }
+
+    onFormSubmit = e => {
+        e.preventDefault();
+
+        //callback
+    }
+
     render() {
-        return <div>SearchBar</div>
+        return (
+            <div className="ui segment search-bar">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input value={this.state.term} type="text" onChange={this.onInputChange} />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
